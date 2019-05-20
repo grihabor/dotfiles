00ec97d547cc545304b769e3ec7289d6cdbd9012
Borodin Gregory <grihabor@gmail.com>
2019-05-20 13:42:03 +0300

    Add script to reinitialize git projects

---
+++ git_init_subdirectories.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+target_directory=$1
+
+if [ "$target_directory" = "" ]
+then
+    echo "Usage: $0 TARGET_DIRECTORY"
+    exit 1
+fi
+
+projects=$(ls -d $target_directory/*/)
+
+for project in $projects
+do
+    path=$(realpath $project)
+    cd $path
+    git init
+    cd ..
+done
