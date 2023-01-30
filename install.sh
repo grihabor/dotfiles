c36f32f7d442988a6fdc4046e25cacf30d26c5f6
Borodin Gregory <grihabor@gmail.com>
2023-01-30 13:20:25 +0400

    Use python scripts

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-TEMPLATE_DIR=$HOME/.git-templates/
-mkdir -p $TEMPLATE_DIR
-
-cp -r ./.git-templates/* $TEMPLATE_DIR
-find $TEMPLATE_DIR
-
-git config --global init.templateDir $TEMPLATE_DIR
