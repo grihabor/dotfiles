642f7bc3a67d4160bf5584697ad02788962e7ff9
Borodin Gregory <grihabor@gmail.com>
2023-12-09 15:21:34 +0100

    Configure pulsepoint specific stuff

---
+++ pulsepoint/.bashrc.d/pp-kubectl-alias.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# kubectl reporting-api aliases
+alias kure="kubectl --context dm-dev -n rapi-regression"
+alias kupr="kubectl --context dm-dev -n rapi-prerelease"
+alias kuqa="kubectl --context dm-dev -n rapi-qa"
+alias kupd="kubectl --context dm-prod -n reporting-api"
+
+# enable complete alias script
+. ~/projects/complete-alias/complete_alias
+
+# patch aliases to make completion work
+complete -F _complete_alias kure
+complete -F _complete_alias kupr
+complete -F _complete_alias kuqa
+complete -F _complete_alias kupd
+
