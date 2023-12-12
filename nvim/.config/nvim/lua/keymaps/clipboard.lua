a6dfc5085a41bcdc1007b48951f9078f98c84d12
Borodin Gregory <grihabor@gmail.com>
2023-12-12 21:42:05 +0100

    Revert "Map clipboard"
    This reverts commit b5abffadeb22dc8ca45d34d6b9f941fe4b611a11.

--- nvim/.config/nvim/lua/keymaps/clipboard.lua
+++
@@ -1,9 +0,0 @@
--- Copy to clipboard
-vim.keymap.set({'n', 'v'}, '<leader>y', '"+y')
-vim.keymap.set('n', '<leader>Y', '"+yg_')
-vim.keymap.set('n', '<leader>yy', '"+yy')
-
--- Paste from clipboard
-vim.keymap.set({'n', 'v'}, '<leader>p', '"+p')
-vim.keymap.set({'n', 'v'}, '<leader>P', '"+P')
-
