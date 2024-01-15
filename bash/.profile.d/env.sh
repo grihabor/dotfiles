c9c1464891ae3994fb09fc27a8a2a8bc184bac04
Borodin Gregory <grihabor@gmail.com>
2024-01-15 23:59:36 +0100

    Configure wrapper script for firefox

--- bash/.profile.d/env.sh
+++ bash/.profile.d/env.sh
@@ -2,10 +2,8 @@ if [ -f "$HOME/.cargo/env" ]; then
     . "$HOME/.cargo/env"
 fi
 
-export PATH="$PATH:/home/grihabor/.local/share/coursier/bin"
-export PATH="$PATH:/home/grihabor/bin/google-cloud-sdk/bin"
-export PATH="$PATH:/home/grihabor/.npm-packages/bin/"
+export PATH="$PATH:$HOME/.local/share/coursier/bin"
+export PATH="$PATH:$HOME/bin/google-cloud-sdk/bin"
+export PATH="$PATH:$HOME/.npm-packages/bin/"
 export PATH="$PATH:/usr/local/go/bin/"
-export PATH="$PATH:/home/grihabor/go/bin/"
-export PATH="$HOME/lib/apache-maven/apache-maven-3.9.3/bin:$PATH"
-export PATH="$HOME/projects/flutter/bin:$PATH"
+export PATH="$PATH:$HOME/go/bin/"
