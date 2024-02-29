2af9d3b92aef73a740bc450e1609a3a3381b72f5
Borodin Gregory <grihabor@gmail.com>
2024-02-29 11:21:25 +0100

    Grep in subdir

--- nvim/.config/nvim/lua/options/telescope.lua
+++ nvim/.config/nvim/lua/options/telescope.lua
@@ -15,6 +15,36 @@ table.insert(vimgrep_arguments, "!**/.git/*")
 if vim.fn.executable("rg") ~= 1 then
 	error("Command 'ripgrep' not found")
 end
+
+-- https://github.com/nvim-telescope/telescope.nvim/issues/2201#issuecomment-1284691502
+local ts_select_dir_for_grep = function(prompt_bufnr)
+	local action_state = require("telescope.actions.state")
+	local fb = require("telescope").extensions.file_browser
+	local live_grep = require("telescope.builtin").live_grep
+	local current_line = action_state.get_current_line()
+
+	fb.file_browser({
+		files = false,
+		depth = false,
+		attach_mappings = function(prompt_bufnr)
+			require("telescope.actions").select_default:replace(function()
+				local entry_path = action_state.get_selected_entry().Path
+				local dir = entry_path:is_dir() and entry_path or entry_path:parent()
+				local relative = dir:make_relative(vim.fn.getcwd())
+				local absolute = dir:absolute()
+
+				live_grep({
+					results_title = relative .. "/",
+					cwd = absolute,
+					default_text = current_line,
+				})
+			end)
+
+			return true
+		end,
+	})
+end
+
 telescope.setup({
 	defaults = {
 		-- `hidden = true` is not supported in text grep commands.
@@ -25,11 +55,11 @@ telescope.setup({
 		mappings = {
 			i = {
 				["<esc>"] = actions.close,
+				["<M-p>"] = action_layout.toggle_preview,
+				["<C-f>"] = ts_select_dir_for_grep,
 			},
 			n = {
-				["<M-p>"] = action_layout.toggle_preview,
-			},
-			i = {
+				["<C-f>"] = ts_select_dir_for_grep,
 				["<M-p>"] = action_layout.toggle_preview,
 			},
 		},
--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -56,6 +56,10 @@ return require("packer").startup(function(use)
 		tag = "0.1.5",
 		requires = { { "nvim-lua/plenary.nvim" } },
 	})
+	use({
+		"nvim-telescope/telescope-file-browser.nvim",
+		requires = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+	})
 
 	-- color theme
 	-- use 'morhetz/gruvbox'
