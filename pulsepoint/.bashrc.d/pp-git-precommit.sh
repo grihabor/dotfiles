e62dda03b2ace5a7e5b0b57707e712d6205e7b73
Borodin Gregory <grihabor@gmail.com>
2024-03-23 11:18:29 +0100

    Remove outdated pre-commit script

--- pulsepoint/.bashrc.d/pp-git-precommit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export GIT_ORGANIZATION_NAME="pulsepointinc"
-export GIT_ORGANIZATION_USER_NAME="Borodin Gregory"
-export GIT_ORGANIZATION_USER_EMAIL="gborodin@pulsepoint.com"
-export GIT_PERSONAL_USER_NAME="Borodin Gregory"
-export GIT_PERSONAL_USER_EMAIL="grihabor@gmail.com"
