ede7f5317bf6e1537079082a8f15511b259e6d4b
Borodin Gregory <grihabor@gmail.com>
2024-03-31 10:47:35 +0200

    Manage neovim & kubectl in nix

--- install-kubectl.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-KUBECTL="$HOME/.local/bin/kubectl"
-curl -L --output "$KUBECTL" \
-    "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-chmod +x "$KUBECTL"
