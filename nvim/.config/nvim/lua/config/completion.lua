929d99c3b3fbce9413b0ad5f1f9fcd0d26c4d572
Borodin Gregory <grihabor@gmail.com>
2024-04-02 13:04:34 +0200

    Update completion mappings

--- nvim/.config/nvim/lua/config/completion.lua
+++ nvim/.config/nvim/lua/config/completion.lua
@@ -70,17 +70,17 @@ return {
             mapping = {
                 ["<C-p>"] = cmp.mapping.select_prev_item(),
                 ["<C-n>"] = cmp.mapping.select_next_item(),
-                -- Add tab support
-                ["<S-Tab>"] = cmp.mapping.select_prev_item(),
-                ["<Tab>"] = cmp.mapping.select_next_item(),
-                ["<C-S-f>"] = cmp.mapping.scroll_docs(-4),
-                ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-u>"] = cmp.mapping.scroll_docs(4),
                 ["<C-Space>"] = cmp.mapping.complete(),
                 ["<C-e>"] = cmp.mapping.close(),
-                ["<CR>"] = cmp.mapping.confirm({
-                    behavior = cmp.ConfirmBehavior.Insert,
-                    select = true,
-                }),
+                ["<C-y>"] = cmp.mapping(
+                    cmp.mapping.confirm({
+                        behavior = cmp.ConfirmBehavior.Insert,
+                        select = false,
+                    }),
+                    { "i", "c" }
+                ),
             },
             sources = sources,
             window = {
