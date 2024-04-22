914bb5c8e7aef0584532f5f3825b978d8809f671
Borodin Gregory <grihabor@gmail.com>
2024-04-22 22:40:10 +0200

    Add neotest keymaps

--- nvim/.config/nvim/lua/config/neotest.lua
+++ nvim/.config/nvim/lua/config/neotest.lua
@@ -1,7 +1,41 @@
 return {
     "nvim-neotest/neotest",
     dependencies = {
+        "antoinemadec/FixCursorHold.nvim",
         "mrcjkb/rustaceanvim",
+        "nvim-lua/plenary.nvim",
+        "nvim-neotest/nvim-nio",
+        "nvim-treesitter/nvim-treesitter",
+    },
+    keys = {
+        {
+            "<leader>tr",
+            function()
+                require("neotest").run.run()
+            end,
+            desc = "Run the nearest test",
+        },
+        {
+            "<leader>td",
+            function()
+                require("neotest").run.run({ strategy = "dap" })
+            end,
+            desc = "Debug the nearest test",
+        },
+        {
+            "<leader>tn",
+            function()
+                require("neotest").run.stop()
+            end,
+            desc = "Stop the nearest test",
+        },
+        {
+            "<leader>ta",
+            function()
+                require("neotest").run.attach()
+            end,
+            desc = "Attach to the nearest test",
+        },
     },
     config = function()
         local opts = {
