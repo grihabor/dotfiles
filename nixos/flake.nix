e788107d076a6361f29307d64169b2e440c426c3
Borodin Gregory <grihabor@gmail.com>
2024-09-28 22:36:01 +0200

    Use nixos-24.05

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,15 +1,20 @@
 {
-  description = "A very basic flake";
+  description = "A simple NixOS flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
   };
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  } @ inputs: {
+    nixosConfigurations.mighty-nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+      ];
+    };
   };
 }
