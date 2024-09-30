fc8bc4e968c9b0bb7a58e5e6252daf9995831782
Borodin Gregory <grihabor@gmail.com>
2024-09-30 11:43:53 +0200

    Install nix-alien

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,19 +1,26 @@
 {
   description = "A simple NixOS flake";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  inputs.nix-alien.url = "github:thiagokokada/nix-alien";
 
   outputs = {
     self,
     nixpkgs,
+    nix-alien,
     ...
   } @ inputs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
+        ({system, ...}: {
+          environment.systemPackages = with nix-alien.packages.${system}; [
+            nix-alien
+          ];
+          # Optional, needed for `nix-alien-ld`
+          programs.nix-ld.enable = true;
+        })
       ];
     };
   };
