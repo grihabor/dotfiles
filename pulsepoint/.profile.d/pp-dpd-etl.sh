3607fba7c2fc9582ddd692d70c5f99d896604a3c
Borodin Gregory <grihabor@gmail.com>
2024-10-16 18:28:49 +0200

    Add forge/dist/src.python.tools to PATH

--- pulsepoint/.profile.d/pp-dpd-etl.sh
+++ pulsepoint/.profile.d/pp-dpd-etl.sh
@@ -2,3 +2,4 @@
 
 export PATH="$PATH:/home/grihabor/pulsepoint/forge/src/python"
 export PATH="$PATH:/home/grihabor/pulsepoint/forge/src/bash"
+export PATH="$PATH:/home/grihabor/pulsepoint/forge/dist/src.python.tools"
