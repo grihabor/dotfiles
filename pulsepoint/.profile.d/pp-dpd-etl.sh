7496e564dd4cd6bf2c681cc9c3d74323b2a91249
Borodin Gregory <grihabor@gmail.com>
2025-03-17 17:38:26 +0100

    Fix PATH in forge

--- pulsepoint/.profile.d/pp-dpd-etl.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export PATH="$PATH:/home/grihabor/pulsepoint/forge/src/python"
-export PATH="$PATH:/home/grihabor/pulsepoint/forge/src/bash"
-export PATH="$PATH:/home/grihabor/pulsepoint/forge/dist/src.python.tools"
---
+++ pulsepoint/.profile.d/pp-forge.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export PATH="$PATH:/home/grihabor/pulsepoint/forge/master/src/python"
+export PATH="$PATH:/home/grihabor/pulsepoint/forge/master/src/bash"
