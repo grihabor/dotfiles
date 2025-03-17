dfd1c04307a798a3c9fe206417dc71d39bab9016
Borodin Gregory <grihabor@gmail.com>
2025-03-17 17:37:54 +0100

    Upgrade nix

--- nix/.config/home-manager/home.nix
+++ nix/.config/home-manager/home.nix
@@ -94,7 +94,7 @@ in {
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
     # # fonts?
-    (pkgs.nerdfonts.override {fonts = ["JetBrainsMono"];})
+    pkgs.nerd-fonts.jetbrains-mono
 
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
