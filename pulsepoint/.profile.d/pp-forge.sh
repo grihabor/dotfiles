3a8944242778f67260d656567f7d398c769278e6
Borodin Gregory <grihabor@gmail.com>
2025-03-24 15:26:12 +0100

    Update forge PATH

--- pulsepoint/.profile.d/pp-forge.sh
+++ pulsepoint/.profile.d/pp-forge.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export PATH="$PATH:/home/grihabor/pulsepoint/forge/master/src/python"
-export PATH="$PATH:/home/grihabor/pulsepoint/forge/master/src/python/tools"
-export PATH="$PATH:/home/grihabor/pulsepoint/forge/master/src/bash"
+export PATH="$PATH:$HOME/pulsepoint/forge/master/src/bash"
+export PATH="$PATH:$HOME/pulsepoint/forge/master/src/python"
+export PATH="$PATH:$HOME/pulsepoint/forge/master/src/python/tools/bin"
