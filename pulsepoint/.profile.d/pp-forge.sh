2069eea76a663ea7bdfe80996d0967f4077707c3
Borodin Gregory <grihabor@gmail.com>
2025-03-24 12:38:49 +0100

    Add another path

--- pulsepoint/.profile.d/pp-forge.sh
+++ pulsepoint/.profile.d/pp-forge.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 export PATH="$PATH:/home/grihabor/pulsepoint/forge/master/src/python"
+export PATH="$PATH:/home/grihabor/pulsepoint/forge/master/src/python/tools"
 export PATH="$PATH:/home/grihabor/pulsepoint/forge/master/src/bash"
