f6186782462e29794f1658e09497a82f350a45b0
Borodin Gregory <grihabor@gmail.com>
2026-06-09 11:30:35 +0200

    Remember state before calling ts_select_dir

--- nvim/.config/nvim/lua/config/telescope.lua
+++ nvim/.config/nvim/lua/config/telescope.lua
@@ -1,36 +1,40 @@
 -- https://github.com/nvim-telescope/telescope.nvim/issues/2201#issuecomment-1284691502
-local ts_select_dir_for_grep = function(prompt_bufnr)
-    local action_state = require("telescope.actions.state")
-    local fb = require("telescope").extensions.file_browser
-    local live_grep = require("telescope.builtin").live_grep
-    local current_line = action_state.get_current_line()
+local ts_select_dir = function(picker_name)
+    return function(prompt_bufnr)
+        local action_state = require("telescope.actions.state")
+        local fb = require("telescope").extensions.file_browser
+        local current_line = action_state.get_current_line()
 
-    fb.file_browser({
-        files = false,
-        depth = false,
-        hidden = {
-            file_browser = true,
-            folder_browser = true,
-        },
-        attach_mappings = function(prompt_bufnr)
-            require("telescope.actions").select_default:replace(function()
-                local entry_path = action_state.get_selected_entry().Path
-                local dir = entry_path:is_dir() and entry_path or entry_path:parent()
-                local relative = dir:make_relative(vim.fn.getcwd())
-                local absolute = dir:absolute()
-                vim.g.telescope_search_dir = absolute
+        fb.file_browser({
+            files = false,
+            depth = false,
+            hidden = {
+                file_browser = true,
+                folder_browser = true,
+            },
+            attach_mappings = function(prompt_bufnr)
+                require("telescope.actions").select_default:replace(function()
+                    local entry_path = action_state.get_selected_entry().Path
+                    local dir = entry_path:is_dir() and entry_path or entry_path:parent()
+                    local relative = dir:make_relative(vim.fn.getcwd())
+                    local absolute = dir:absolute()
+                    vim.g.telescope_search_dir = absolute
 
-                live_grep({
-                    results_title = relative .. "/",
-                    cwd = absolute,
-                    default_text = current_line,
-                })
-            end)
+                    require("telescope.builtin")[picker_name]({
+                        results_title = relative .. "/",
+                        cwd = absolute,
+                        default_text = current_line,
+                    })
+                end)
 
-            return true
-        end,
-    })
+                return true
+            end,
+        })
+    end
 end
+
+local ts_select_dir_for_grep = ts_select_dir("live_grep")
+local ts_select_dir_for_find_files = ts_select_dir("find_files")
 
 return {
     "nvim-telescope/telescope.nvim",
@@ -75,6 +79,14 @@ return {
                 find_files = {
                     -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
                     find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+                    mappings = {
+                        i = {
+                            ["<C-f>"] = ts_select_dir_for_find_files,
+                        },
+                        n = {
+                            ["<C-f>"] = ts_select_dir_for_find_files,
+                        },
+                    },
                 },
                 help_tags = {
                     mappings = {
