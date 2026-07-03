a72c6d72033df44f12e2ddc8b32235bf28cc511a
Borodin Gregory <grihabor@gmail.com>
2026-07-03 10:48:01 +0200

    Open quickfix at the bottom

--- nvim/.config/nvim/lua/quickfix_command.lua
+++ nvim/.config/nvim/lua/quickfix_command.lua
@@ -143,7 +143,7 @@ local function set_quickfix_list(items, title)
 
     if #items > 0 then
         local win = vim.api.nvim_get_current_win()
-        vim.cmd("copen")
+        vim.cmd("botright copen")
         vim.api.nvim_set_current_win(win)
     end
 end
